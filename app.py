933a4aa6baca525a46e01ce4ab30ffbd954c6f9a
sairam0607 <srr6795@gmail.com>
2022-10-11 13:14:52 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+model = pickle.load(open('Car-Price-Prediction-\random_forest_regression_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
-    return render_template('index.html')
+    return render_template('Car-Price-Prediction-\templates\index.html')
 
 
 standard_to = StandardScaler()
